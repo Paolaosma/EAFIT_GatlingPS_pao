46daadf6043722fe0b99d20b9906f125855d11ba
Paolaosma <33330869+Paolaosma@users.noreply.github.com>
2025-08-16 09:49:33 -0500

    Update Data.scala
    prueba parabank

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,7 +4,12 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "321Paola"
     val password = "123456"
+    val fromAccountId = "17673"
+    val toAccountId = "18894"
+    val amount = "8.76"
+
 
 }
+
 
 
